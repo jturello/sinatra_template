916f89afc521cb655e4c525eddb2260aeca3bf35
jturello <jdturello@gmail.com>
2016-07-07 23:00:42 -0700

    Update template_integration_spec.rb

--- spec/template_integration_spec.rb
+++ spec/template_integration_spec.rb
@@ -1,10 +1,13 @@
-require('rspec')
-require('TEMPLATE')
-
-describe('Fixnum#coin') do
-
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+describe('STRING#TEMPLATE', {:type => :feature}) do
   it('DOES SOMETHING...') do
-    expect((X).TEMPLATE).to(eq(RESULT))
+    visit('/')
+    fill_in('TEMPLATE', :with => 'SOMETHING')
+    click_button('Enter')
+    # save_and_open_page
+    expect(page).to have_content('SOME RESULT')
   end
-
 end
